56457c741247c25c9c70bbab862683fe4f427e36
ubuntu <irenicus30@gmail.com>
2020-03-03 12:49:17 +0100

    first working version

---
+++ src/app.ts
@@ -0,0 +1,31 @@
+import * as express from 'express';
+import * as cors from 'cors';
+import * as bodyparser from 'body-parser';
+
+const app = express();
+app.use(cors());
+app.use(bodyparser.json());
+
+app.get('/todo', (req: express.Request, res: express.Response, next: express.NextFunction) => {
+  res.status(200);
+  res.json([{id: 1, description: 'Buy Bread'}]);
+});
+app.post('/todo', (req: express.Request, res: express.Response, next: express.NextFunction) => {
+  console.info(`${JSON.stringify(req.body)}`);
+  res.status(200);
+  res.end();
+});
+app.put('/todo/:id', (req: express.Request, res: express.Response, next: express.NextFunction) => {
+  console.info(`${JSON.stringify(req.body)}`);
+  console.info(`:id = ${req.params.id}`);
+  res.status(200);
+  res.end();
+});
+app.delete('/todo/:id', (req: express.Request, res: express.Response, next: express.NextFunction) => {
+  console.info(`:id = ${req.params.id}`);
+  res.status(200);
+  res.end();
+});
+
+export { app }
+
