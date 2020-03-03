28669c65a339907fa01376976dd3e93836b82ba7
ubuntu <irenicus30@gmail.com>
2020-03-03 19:37:53 +0100

    connect to mlab database

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import * as http from 'http';
 import * as mongoose from "mongoose";
 
 const PORT = process.env.PORT || 8080;
-const MONGO_URI = process.env.MONGODB_URI || 'mongodb://10.0.0.129:27017/todo';
+const MONGO_URI = process.env.MONGODB_URI || 'mongodb://test:test1!@ds145346.mlab.com:45346/movies54698';
 
 const server = http.createServer(app);
 
@@ -11,6 +11,7 @@ server.listen(PORT);
 server.on('error', (err) => {
   console.error(err);
 });
+
 server.on('listening', async () => {
     console.info(`Listening on port ${PORT}`);
     mongoose.connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true }).then(
